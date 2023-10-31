4e797fa86d373988403c816d9cbedbf44580e74f
Octavio Thomsen <otithomsen99@gmail.com>
2023-10-31 18:33:07 -0300

    Cambio routing a página de inicio y hago su implementación inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CursoListComponent } from './components/curso-list/curso-list.component';
 import { CursoDetailsComponent } from './components/curso-details/curso-details.component';
 import { CursoAddComponent } from './components/curso-add/curso-add.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'cursos', pathMatch: 'full' },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
   { path: 'cursos', component: CursoListComponent },
   { path: 'cursos/:id', component: CursoDetailsComponent },
   { path: 'add', component: CursoAddComponent }
